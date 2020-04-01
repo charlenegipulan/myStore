b91a378a8b676136647db7c9f2ef7f1e92b2fe02
Charlene Gipulan <charlenegipulan@gmail.com>
2020-03-31 21:30:56 -0700

    setup of views and controllers

---
+++ app/templates/templates.js
@@ -0,0 +1,13 @@
+angular.module('myStore.templates',['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider){
+    $routeProvider.
+    when('/templates', {
+        templateUrl: 'templates/templates.html',
+        controller: 'TemplatesCtrl'
+    });
+}])
+
+.controller('TemplatesCtrl', ['$scope', function($scope) {
+    console.log($scope);
+}]);
